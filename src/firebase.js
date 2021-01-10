c9b6b55352afe6751b6bb38eac4f531420efd975
kshivam99 <sawarn101@gmail.com>
2021-01-10 23:02:44 +0530

    home page UI

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDexX5jxoHRYH6F6NPtPtdTtAXs_bl2Bc",
+  authDomain: "challenge-6bece.firebaseapp.com",
+  projectId: "challenge-6bece",
+  storageBucket: "challenge-6bece.appspot.com",
+  messagingSenderId: "3003480243",
+  appId: "1:3003480243:web:9bfc0bbe3007bc1047d8c6",
+  measurementId: "G-MNZBZKFGY3"
+};
